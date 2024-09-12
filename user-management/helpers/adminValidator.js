b4869ba973e6173533ffcdf6de9323373f3322c2
mariamawitseifu <merrybee34@gmail.com>
2024-09-12 11:43:05 -0700

    crd done and update started

--- user-management/helpers/adminValidator.js
+++ user-management/helpers/adminValidator.js
@@ -1,6 +1,14 @@
 const { check, validationResult } = require('express-validator');
 
 exports.permissionAddValidator = [
-    check('permission_name', 'Permission Name is required').notEmpty(),
+    check('permission_name', 'Permission Name is required').not().isEmpty(),
+];
+
+exports.permissionDeleteValidator = [
+    check('id', 'ID is required').not().isEmpty(),
+];
+exports.permissionUpdateValidator = [
+    check('id', 'ID is required').not().isEmpty(),
+    check('permission_name', 'Permission Name is required').not().isEmpty(),
 ];
 
--- user-management/routes/adminRoute.js
+++ user-management/routes/adminRoute.js
@@ -4,10 +4,13 @@ const router = express.Router();
 const permissionController = require('../controllers/admin/permissionController');
 
 const auth = require('../middlewares/authMiddleware')
-const { permissionAddValidator } = require('../helpers/adminValidator')
+const { permissionAddValidator, permissionDeleteValidator, permissionUpdateValidator } = require('../helpers/adminValidator')
 
 
 router.post('/add-permission', permissionAddValidator,permissionController.addPermission);
+router.get('/get-permission', permissionController.getPermissions);
+router.get('/delete-permission', permissionDeleteValidator,permissionController.deletePermission);
+router.get('/update-permission', permissionUpdateValidator,permissionController.updatePermission);
 
 
 module.exports = router;
