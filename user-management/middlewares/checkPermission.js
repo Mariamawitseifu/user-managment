5f8a4c96db4aa903db558df2bc8bade794d3b9fb
mariamawit <merrybee34@gmail.com>
2024-09-21 08:52:21 +0300

    checkpermission

--- user-management/middlewares/checkPermission.js
+++ user-management/middlewares/checkPermission.js
@@ -5,13 +5,25 @@ const checkPermission = async (req,res, next) => {
             const routerPermission = await helper.getRouterPermission(req.path, req.user.role);
             const userPermission = await helper.getRouterPermission(req.user._id);
 
-            if(!routerPermission){
+            if(userPermissions.permissions.permissions == undefined || !routerPermission){
                 return res.status(400).json({
                     success: false,
                     msg: "You haven't permission to access this route!"
                 });
             }
+            const permission_name = routerPermission.permission_id.permission_name;
+            const permission_values = routerPermission.permission;
 
+            const hasPermission = userPermissions.permissions.permissions.some(permission =>
+                permission.permission_name == permission_name &&
+                permission.permission_value.some(value => permission_values.includes(value))
+            );
+            if(!hasPermission){
+                return res.status(400).json({
+                    success: false,
+                    msg: "You haven't permission to access this route!"
+                });
+            }
         }
         return next();
 
